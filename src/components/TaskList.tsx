faf8a9357d5c51974305ade709aa787170fe8688
Kelson Teixeira <kelson.teixeira@camara.leg.br>
2022-03-17 01:07:52 -0300

    desafio finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,34 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle !== ''){
+      const newTask: Task = {
+        id: Math.floor(Math.random() * 1000),
+        title: newTaskTitle,
+        isComplete: false
+      }
+
+      setTasks([...tasks, newTask]);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTaskList: Task[] = tasks.map(task => {
+      if(task.id === id){
+        task.isComplete = !task.isComplete;
+        return task;
+      }else{
+        return task;
+      }
+    });
+
+    setTasks(newTaskList);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const newTaskList: Task[] = tasks.filter(task => task.id !== id);
+
+    setTasks(newTaskList);
   }
 
   return (
